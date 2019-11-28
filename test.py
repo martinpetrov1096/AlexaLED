92f46b5fc221575978f278e5a5cb4f1af46d4c2e
Martin Petrov <mlpetrov@ucdavis.edu>
2019-11-27 17:45:19 -0800

    added test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import http
+import socket
 import urllib.request
-
-
 
 req = urllib.request.Request(
     url="http://192.168.1.14:8080/status",
@@ -11,7 +10,7 @@ req = urllib.request.Request(
 with urllib.request.urlopen(req) as resp:
     response_content = resp.read().decode("utf8")
 
-if "off" in response_content:
+if "OFF" in response_content:
     print ("Off")
-
-print ("hello")
+elif "ON" in response_content:
+    print("On")

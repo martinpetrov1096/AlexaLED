8e87acb70cbe55c4029a77f7522920c79cfb40c7
Martin Petrov <mlpetrov@ucdavis.edu>
2019-12-15 22:52:27 -0800

    added toggle for brightness control

--- led_page.py
+++ led_page.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, json
 #from gpiozero import LED
 app = Flask(__name__)
 #led = LED(27)
-
+currBrightness = 0 #TODO: set to current led brightness
+status = 0
 
 @app.route("/on")
 def test():
@@ -23,9 +24,36 @@ def status():
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print(currBrightness)
+    return render_template("main.html", brightness=currBrightness)
+
+
+
+
+
+#background processses that happen without any refreshing
+
+
+#Toggle Power
+#@app.route('/toggle_power', methods=['POST'])
+#def toggle_power():
+
+
+
+#Change brightness
+@app.route('/set_brightness',methods=['GET', 'POST'])
+def setBrightness():
+    if request.method == 'POST':
+        currBrightness = int(request.form['myBrightness'])
+    
+    print(currBrightness) #TODO: set LED to new brightness
+
+    #Returns variable to update page with current brightness
+    return json.jsonify({
+        'newBrightness': currBrightness
+    })
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, threaded=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=False, use_reloader=False)
 

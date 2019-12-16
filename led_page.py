06a42676d703b9af38080756136171653297ef6e
Martin Petrov <mlpetrov@ucdavis.edu>
2019-12-15 19:30:12 -0800

    removed .venv. About to recreate and clean up

--- led_page.py
+++ led_page.py
@@ -1,25 +1,25 @@
 from flask import Flask, request, render_template
-from gpiozero import LED
+#from gpiozero import LED
 app = Flask(__name__)
-led = LED(27)
+#led = LED(27)
 
 
 @app.route("/on")
 def test():
-    led.on()
+#    led.on()
     return render_template("on.html")
 
 @app.route("/off")
 def test2():
-    led.off()
+#    led.off()
     return render_template("off.html")
 
 @app.route("/status")
 def status():
-    if(led.is_lit):
-        return render_template("on.html")
-    else:
-        return render_template("off.html")
+#    if(led.is_lit):
+#        return render_template("on.html")
+#    else:
+    return render_template("off.html")
 
 @app.route("/")
 def hello():
